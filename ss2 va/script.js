7dc1d1ed8333ea660e43a04ad5d4b73648b8401b
anh <anhtl.dtvt@gmail.com>
2020-02-15 21:45:10 +0700

    code

---
+++ ss2 va/script.js
@@ -0,0 +1,39 @@
+// alert("xin chao c4ejs 71") // thong bao
+
+// prompt("ten ban la gi?"); // nhap thong tin
+
+let age; // khai bao bien
+age = 25; // gan gia tri
+
+// 25 = age; ai
+
+let age1 = 25;
+
+// su dung
+// alert(age); 
+// alert(age1);
+
+console.log(age);
+console.log(age1);
+
+
+var name;
+const PI = 3.14;
+
+let a = 5;
+let b = 10;
+
+let c = a + b;
+console.log(c);
+
+// - * /
+
+let c = a - b;
+console.log(c);
+ 
+let c = a * b;
+console.log(c);
+
+let c = a / b;
+console.log(c);
+
